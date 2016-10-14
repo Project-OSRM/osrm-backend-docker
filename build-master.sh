bc04b874cc2f7f12ef7123381a1cf783cce50d78
Daniel Patterson <daniel@mapbox.com>
2016-10-14 00:45:21 -0600

    Make build script generic - it can do release builds too.

--- build-master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-# Get the current HEAD gitsha, and use the first 8 chars as a short reference to it
-SHORT_HEAD=$(git ls-remote https://github.com/Project-OSRM/osrm-backend.git HEAD | cut -f 1 | cut -c1-8 )
-
-docker build -t osrm:master-${SHORT_HEAD} --build-arg OSRM_VERSION=${SHORT_HEAD} --build-arg BUILD_TYPE=Release .
-docker build -t osrm:master-${SHORT_HEAD}-debug --build-arg OSRM_VERSION=${SHORT_HEAD} --build-arg BUILD_TYPE=Debug .
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -eq 0 ] ; then
+    # Get the current HEAD gitsha, and use the first 8 chars as a short reference to it
+    REV=$(git ls-remote https://github.com/Project-OSRM/osrm-backend.git HEAD | cut -f 1 | cut -c1-8 )
+    PREFIX="master-"
+    echo "No tag/gitsha/branch supplied, using master@${REV}"
+else
+    # Use whatever value the caller supplied
+    REV=$1
+    PREFIX=""
+fi
+
+docker build -t osrm:${PREFIX}${REV} --build-arg OSRM_VERSION=${REV} --build-arg BUILD_TYPE=Release .
+docker build -t osrm:${PREFIX}${REV}-debug --build-arg OSRM_VERSION=${REV} --build-arg BUILD_TYPE=Debug .
